29a23aa2ff4ba274883adc5eb84652db042afcbc
Mariano Koci <mariano.koci@seciu.edu.uy>
2024-08-24 02:33:39 -0300

    Pasaje a useReducer

--- src/components/OrderTotals.tsx
+++ src/components/OrderTotals.tsx
@@ -1,14 +1,15 @@
-import { useMemo } from "react"
+import { useMemo, Dispatch } from "react"
 import { OrderItem } from "../types"
 import { formatCurrency } from "../helpers"
+import { OrderActions } from "../reducers/order-reducer"
 
 type OrderTotalsProps = {
   order: OrderItem[]
   tip: number
-  placeOrder: () => void
+  dispatch: Dispatch<OrderActions>
 }
 
-export default function OrderTotals({order, tip, placeOrder} : OrderTotalsProps) {
+export default function OrderTotals({order, tip, dispatch} : OrderTotalsProps) {
 
   const subTotalAmount = useMemo(() => order.reduce( (total, item) => total + (item.quantity * item.price),0), [order])
   const tipAmount = useMemo(() => subTotalAmount * tip, [tip, order])
@@ -34,7 +35,7 @@ export default function OrderTotals({order, tip, placeOrder} : OrderTotalsProps)
       <button
         className="w-full bg-black p-3 text-white font-bold mt-10 disabled:opacity-10"
         disabled={totalAmount === 0}
-        onClick={placeOrder}
+        onClick={()=>dispatch({type: 'place-order'})}
       >
         Guardar Orden
       </button>
